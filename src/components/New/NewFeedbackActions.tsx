b070ee203cdb998cafc3b4f8d50ad9b842b8a906
jeeasistio <jeeasistio08@gmail.com>
2023-04-15 02:19:05 +0800

    fixed new feedback actions mobile view

--- src/components/New/NewFeedbackActions.tsx
+++ src/components/New/NewFeedbackActions.tsx
@@ -2,7 +2,7 @@ import { Button } from "../Utils/Button"
 
 export const NewFeedbackActions = () => {
     return (
-        <div className="flex justify-between">
+        <div className="flex flex-col justify-between gap-4 sm:flex-row">
             <Button color="red">Delete</Button>
 
             <div className="flex flex-col justify-end gap-4 sm:flex-row">
